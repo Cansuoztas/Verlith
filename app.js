c8514e2ae915fc791012b7cdf9ec0d073814953b
Cansu Öztaş <fcansuoztas@gmail.com>
2022-11-20 11:14:03 +0300

    edit

---
+++ app.js
@@ -0,0 +1,52 @@
+
+
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    loop: true,
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: -0.1,
+      stretch: -15,
+      depth: 100,
+      modifier: 1.68,
+      slideShadows: false,
+      
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    keyboard: {
+      enabled: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+
+  var swiper = new Swiper(".mySwiperfirst", {
+    effect: "coverflow",
+    loop: true,
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 20,
+      stretch: 5,
+      depth: 180,
+      modifier: 1,
+      slideShadows: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    keyboard: {
+      enabled: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
