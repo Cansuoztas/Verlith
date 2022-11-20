44215f0f848c6d83132c2bd1d2a1573a7a792222
Cansu Öztaş <fcansuoztas@gmail.com>
2022-11-20 16:47:23 +0300

    edit

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var swiper = new Swiper(".mySwiper", {
     coverflowEffect: {
       rotate: -0.1,
       stretch: -15,
-      depth: 80,
+      depth: 50,
       modifier: 1.68,
       slideShadows: false,
       
@@ -35,7 +35,7 @@ var swiper = new Swiper(".mySwiper", {
     coverflowEffect: {
       rotate: 10,
       stretch: 5,
-      depth: 80,
+      depth: 100,
       modifier: 1,
       slideShadows: false,
     },
